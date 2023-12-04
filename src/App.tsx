fea0673ca8539aa9a153379393b9bd0514100963
yingpengsha <yingpengsha@kuaishou.com>
2023-12-04 14:57:19 +0800

    chore: add baidu map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Map, APILoader } from "@uiw/react-baidu-map";
+
 function App() {
-  return <div className="text-3xl font-bold underline">中午吃什么</div>;
+  return (
+    <div className="max-w-xl p-8 flex flex-col gap-50">
+      <div className="relative pb-[100%] box-border">
+        <div className="absolute top-0 left-0 right-0 bottom-0 rounded-xl shadow-xl shadow-light-400 overflow-hidden" id="map-container">
+          <APILoader akay={import.meta.env.VITE_BAIDU_MAP_AK}>
+            <Map center="杭州 EFC" />
+          </APILoader>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
